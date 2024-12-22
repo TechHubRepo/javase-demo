41388368b7f7e63e50ed6ae068a412d102e1d6a9
Ram Niwash <rniwash122@gmail.com>
2024-12-22 23:10:19 +0530

    Added JLIND DEMO

---
+++ JavaSE-9/JLINK/src/module.a/p1/Hello.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("APPLICATION STARTED");
+        for(int i = 1;i<=10;i++){
+            System.out.println("I = "+i);
+        }
+        System.out.println("APPLICATION ENDED");
+    }
+}
