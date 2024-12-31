004d870be0756ec8c84c4f018b04ddbadfbbb278
Ram Niwash <rniwash122@gmail.com>
2024-12-31 22:05:50 +0530

    Try with resource enhencenment updateed

--- JavaSE-9/Java9_Features/src/main/java/my/java9/features/tryresenhence/Java1_6.java
+++ JavaSE-9/Java9_Features/src/main/java/my/java9/features/tryresenhence/Java1_6.java
@@ -8,14 +8,17 @@ public class Java1_6 {
     private static final Logger LOG = Logger.getLogger("Java1.6 Logger");
 
     public static void main(String[] args) {
-        MyResource myResource = new MyResource("Java 1.6 Resource");
+        MyResource  myResource = null;
         try{
+            myResource = new MyResource("Java 1.6 Resource");
             String resName = myResource.getName();
             LOG.log(Level.INFO,resName);
         }catch (MyResourceException exception){
             LOG.log(Level.SEVERE, exception.getMessage(),exception);
         }finally {
-            myResource.close();
+            if(myResource != null) {
+                myResource.close();
+            }
         }
     }
 }
